db6407786d70350e71f8cb234b12c89445f4161b
Nitin Vaghani <nitinvaghani34@gmail.com>
2019-05-31 15:05:52 +0530

    Create Laravel-Constant.php
    Laravel Constants

---
+++ Laravel-Constant.php
@@ -0,0 +1,32 @@
+<?php
+//create constant.php in app/config folder
+
+return [
+    /*
+       | Array of all game types
+      |
+     */
+    'game_types' => [
+        'treasure_hunt' => 1,
+        'fitness_classes' => 2,
+        'guided_tours' => 3,
+        'quiz' => 4
+    ],
+    'banned_words' => [
+        "aaa",
+        "bbb",
+        "ccc"
+    ]
+];
+
+
+//get data from constant file:
+
+@php 
+$banned_words = json_encode(config('constants.banned_words'));
+@endphp
+
+        
+<script>
+var banned_words=JSON.parse('{!! $banned_words!!}');
+</script>
