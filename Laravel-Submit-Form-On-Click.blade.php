1ebc52a888a20047eec145bbc7661940df6f127b
Nitin Vaghani <nitinvaghani34@gmail.com>
2019-05-31 14:43:41 +0530

    Create Laravel-Submit-Form-On-Click.blade.php
    Laravel - Submit Form On Click

---
+++ Laravel-Submit-Form-On-Click.blade.php
@@ -0,0 +1,12 @@
+<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+    <a class="dropdown-item" href="{{ route('products.index') }}">Products</a>
+    <a class="dropdown-item" href="{{ route('logout') }}"
+       onclick="event.preventDefault();
+               document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}
+    </a>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+</div>
